ef398319a905838805246c64939c9d760a1880dd
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 19:59:38 +0530

    Initial commit

---
+++ StringIndexing.py
@@ -0,0 +1,13 @@
+"""---------- String Indexing ---------------
+
+---> Process of obtaining a perticular charecter
+---> By passing valid existing position
+     otherwise we get an error called IndexError(string index out of range) """
+
+s="PYTHON"
+
+print(s[1])
+print(s[-1])
+print(s[3])
+print(s[-3])
+#print(s[30]) ---> it gives IndexError
